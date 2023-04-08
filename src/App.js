ad0eeeb79221ac07dfb9f51434ef4dbfd81d3425
mannan49 <mannannasir49@gmail.com>
2023-04-08 10:33:46 +0500

    Changes Browser Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import GearTrains from "./Components/GearTrains";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
@@ -7,14 +7,16 @@ import Details from "./Components/Details";
 function App() {
   return (
     <>
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<GearTrains />} />
-        <Route path="/details" element={<Details />} />
-        <Route path= "/about" element={<About />} />  
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<GearTrains />} />
+            <Route path="/details" element={<Details />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
