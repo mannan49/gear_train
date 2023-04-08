2e7efacd7beac7542690aec78589334510539c34
mannan49 <mannannasir49@gmail.com>
2023-04-08 10:37:44 +0500

    Added BaseName

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <BrowserRouter>
+        <BrowserRouter basename="/gear_train">
           <Routes>
             <Route path="/" element={<GearTrains />} />
             <Route path="/details" element={<Details />} />
